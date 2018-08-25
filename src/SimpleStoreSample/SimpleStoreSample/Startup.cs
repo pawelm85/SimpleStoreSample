dc6ccce2cd8678b4b7745365a4cf0ca3b8d36ad2
Paweł <pawelm85@yahoo.pl>
2018-08-25 14:42:14 +0200

    initial-commit

---
+++ src/SimpleStoreSample/SimpleStoreSample/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SimpleStoreSample.Startup))]
+namespace SimpleStoreSample
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
